f098b8748fed36a1f287c09ebff2ec5c9ff67192
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-09-30 15:05:36 +0000

    chore: advanced ssr

--- server.js
+++ server.js
@@ -4,7 +4,36 @@ import { fileURLToPath } from 'url'
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+async function createProdServer() {
+  const app = express()
+  app.use((await import('compression')).default())
+  app.use(
+    (await import('serve-static')).default(
+      path.resolve(__dirname, 'dist/client'),
+      {
+        index: false,
+      },
+    ),
+  )
+  app.use('*', async (req, res, next) => {
+    try {
+      let template = fs.readFileSync(
+        path.resolve(__dirname, 'dist/client/index.html'),
+        'utf-8',
+      )
+      const render = (await import('./dist/server/entry-server.js')).render
+      const appHtml = await render(req)
+      const html = template.replace(`<!--ssr-outlet-->`, appHtml)
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+    } catch (e) {
+      next(e)
+    }
+  })
+  return app
+}
+
 async function createDevServer() {
   const app = express()
   const vite = await (
@@ -35,7 +64,19 @@ async function createDevServer() {
   })
   return app
 }
-const app = await createDevServer()
-app.listen(process.env.PORT, () =>
-  console.log(`ssr dev server running on http://localhost:${process.env.PORT}`),
-)
+
+if (process.env.NODE_ENV === 'production') {
+  const app = await createProdServer()
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `ssr production server running on http://localhost:${process.env.PORT}`
+    )
+  })
+} else {
+  const app = await createDevServer()
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `ssr dev server running on http://localhost:${process.env.PORT}`
+    )
+  })
+}
