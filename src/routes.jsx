c080b4dd9dd13bbf4baa327f38e696b16bad8208
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-09-30 15:34:54 +0000

    chore: prepare for seo

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,8 @@ import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
 import { useLoaderData } from 'react-router-dom'
-import { getPosts } from './api/posts.js'
+import { getPosts, getPostById } from './api/posts.js'
+import { ViewPost } from './pages/ViewPost.jsx'
 import {
   QueryClient,
   dehydrate,
@@ -50,5 +51,33 @@ export const routes = [
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/posts/:postId',
+    loader: async ({ params }) => {
+      const postId = params.postId
+      const queryClient = new QueryClient()
+      const post = await getPostById(postId)
+      await queryClient.prefetchQuery({
+        queryKey: ['post', postId],
+        queryFn: () => post,
+      })
+      if (post?.author) {
+        await queryClient.prefetchQuery({
+          queryKey: ['users', post.author],
+          queryFn: () => getUserInfo(post.author),
+        })
+      }
+      return { dehydratedState: dehydrate(queryClient), postId }
+    },
+
+    Component() {
+      const { dehydratedState, postId } = useLoaderData()
+      return (
+        <HydrationBoundary state={dehydratedState}>
+          <ViewPost postId={postId} />
+        </HydrationBoundary>
+      )
+    },
   },
 ]
