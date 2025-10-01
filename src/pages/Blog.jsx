b8478eda527080a949314aad033cf4323fd46f3a
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-10-01 15:13:40 +0000

    chore: meta tags

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -20,6 +20,10 @@ export function Blog() {
     <div style={{ padding: 8 }}>
       <Helmet>
         <title>Full-Stack React Blog</title>
+        <meta
+          name='description'
+          content='A blog full of articles about full-stack React development.'
+        />
       </Helmet>
       <Header />
       <br />
--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -5,17 +5,29 @@ import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
 import { getPostById } from '../api/posts.js'
 import { Helmet } from 'react-helmet-async'
+// eslint-disable-next-line no-unused-vars
+import { PostList } from '../components/PostList.jsx'
 export function ViewPost({ postId }) {
   const postQuery = useQuery({
     queryKey: ['post', postId],
     queryFn: () => getPostById(postId),
   })
   const post = postQuery.data
+  function truncate(str, max = 160) {
+    if (!str) return str
+    if (str.length > max) {
+      return str.slice(0, max - 3) + '...'
+    } else {
+      return str
+    }
+  }
+
   return (
     <div style={{ padding: 8 }}>
       {post && (
         <Helmet>
           <title>{post.title} | Full-Stack React Blog</title>
+          <meta name='description' content={truncate(post.contents)} />
         </Helmet>
       )}
       <Header />
