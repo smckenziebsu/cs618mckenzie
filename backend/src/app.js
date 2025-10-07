39598aa58111c54b8f3a409d31cc71297789b4f0
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-10-07 14:45:15 +0000

    chore: event model service and routes in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,13 @@ import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { eventRoutes } from './routes/events.js'
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
 })
