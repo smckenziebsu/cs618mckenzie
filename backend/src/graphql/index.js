60d653bd29b796778bb87669d3df782b3373780b
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-10-09 14:19:42 +0000

    chore: implement types for posts users and inputs

--- backend/src/graphql/index.js
+++ backend/src/graphql/index.js
@@ -1,3 +1,5 @@
 import { querySchema, queryResolver } from './query.js'
-export const typeDefs = [querySchema]
-export const resolvers = [queryResolver]
+import { postSchema, postResolver } from './post.js'
+import { userSchema, userResolver } from './user.js'
+export const typeDefs = [querySchema, postSchema, userSchema]
+export const resolvers = [queryResolver, postResolver, userResolver]
