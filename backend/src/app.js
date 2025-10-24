00b4b2387d25b2483b6ab8998360fcbb37939d82
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-10-24 18:01:53 +0000

    chore: integrate socket.io client with react

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -9,7 +8,6 @@ import { handleSocket } from './socket.js'
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
-postsRoutes(app)
 userRoutes(app)
 app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,9 @@ import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import { io } from 'socket.io-client'
+import { SocketIOContextProvider } from './contexts/SocketIOContext.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-const socket = io(import.meta.env.VITE_SOCKET_HOST, {
-  query: 'room=' + new URLSearchParams(window.location.search).get('room'),
-  auth: {
-    token: new URLSearchParams(window.location.search).get('token'),
-  },
-})
+
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
@@ -26,26 +21,14 @@ const router = createBrowserRouter([
     element: <Login />,
   },
 ])
-socket.on('connect', async () => {
-  console.log('connected to socket.io as', socket.id)
-  socket.emit(
-    'chat.message',
-    new URLSearchParams(window.location.search).get('mymessage'),
-  )
-  const userInfo = await socket.emitWithAck('user.info', socket.id)
-  console.log('user info', userInfo)
-})
-socket.on('connect_error', (err) => {
-  console.error('socket.io connect error:', err)
-})
-socket.on('chat.message', (msg) => {
-  console.log(`${msg.username}: ${msg.message}`)
-})
+
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <RouterProvider router={router} />
+        <SocketIOContextProvider>
+          <RouterProvider router={router} />
+        </SocketIOContextProvider>
       </AuthContextProvider>
     </QueryClientProvider>
   )
